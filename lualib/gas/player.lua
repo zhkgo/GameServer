5570ff59773a891c238741edebb8939ad5a59683
zhkgo <zhkgogogo@foxmail.com>
2024-09-01 21:46:55 +0800

    初始化玩家登录管理

---
+++ lualib/gas/player.lua
@@ -0,0 +1,39 @@
+Player = {}
+
+function Player:new(...)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    o:Ctor(...)
+    return o
+end
+
+function Player:Ctor(uid, sid)
+    self.m_Uid = uid
+    self.m_Sid = sid
+    self.m_Invaild = false
+end
+
+function Player:Invaild()
+    return self.m_Invaild
+end
+
+function Player:Reconnect()
+    print("Player:Reconnect", self.m_Uid)
+end
+
+function Player:Connect()
+    print("Player:Connect", self.m_Uid)
+    self:LoadFromDB()
+end
+
+function Player:LoadFromDB()
+    print("Player:LoadFromDB", self.m_Uid)
+    self.m_Name = "UID" .. tostring(self.m_Uid)
+    self.m_Invaild = true
+end
+
+function Player:SaveToDB()
+    print("Player:SaveToDB", self.m_Uid)
+end
+
---
+++ uno-src/GameServer.lua
@@ -0,0 +1,58 @@
+local skynet = require "skynet"
+require "gas.playerMgr"
+
+skynet.register_protocol {
+	name = "client",
+	id = skynet.PTYPE_CLIENT,
+	unpack = skynet.tostring,
+}
+
+SOURCE_GATE = nil
+CmdMgr = {}
+
+function CmdMgr.login(source, uid, sid, secret)
+	-- you may use secret to make a encrypted data stream
+	skynet.error(string.format("%s is login", uid))
+	if not SOURCE_GATE then
+		SOURCE_GATE = source
+	elseif SOURCE_GATE ~= source then
+		skynet.error("SOURCE_GATE is conflict")
+		return
+	end
+
+	PlayerMgr:Add(uid, sid)
+	-- you may load user data from database
+end
+
+-- 玩家离线暂不处理
+function CmdMgr.logout(source, userid)
+	-- NOTICE: The logout MAY be reentry
+	skynet.error(string.format("%s is logout", userid))
+	local player = PlayerMgr:GetById(userid)
+	if player and SOURCE_GATE then
+		skynet.call(SOURCE_GATE, "lua", "logout", userid, player.m_Sid)
+	end
+	PlayerMgr:Logout(userid)
+end
+
+function CmdMgr.afk(source, userid)
+	-- the connection is broken, but the user may back
+	skynet.error(string.format("AFK %s", userid))
+
+	-- 玩家离线 隔一段时间后踢出 TODO
+end
+
+skynet.start(function()
+	-- If you want to fork a work thread , you MUST do it in CMD.login
+	skynet.dispatch("lua", function(session, source, command, ...)
+		local f = assert(CmdMgr[command])
+		skynet.ret(skynet.pack(f(source, ...)))
+	end)
+
+	skynet.dispatch("client", function(_,_, msg)
+		-- the simple echo service
+		print(msg)
+		skynet.sleep(1000000)	-- sleep a while
+		skynet.ret(msg)
+	end)
+end)
